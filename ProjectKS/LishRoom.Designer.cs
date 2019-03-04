43abcfb12c3b5e6f252e4942ac115d17efac7d1e
Tuanhp12 <tuanptd00724@fpt.edu.vn>
2019-03-04 19:17:11 +0700

    Hotel

--- ProjectKS/LishRoom.Designer.cs
+++ ProjectKS/LishRoom.Designer.cs
@@ -28,51 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(189, 6);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(141, 21);
-            this.comboBox2.TabIndex = 42;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(99, 13);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(69, 30);
-            this.label7.TabIndex = 41;
-            this.label7.Text = "Status";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(-57, 7);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(141, 20);
-            this.textBox4.TabIndex = 40;
             // 
             // label4
             // 
@@ -84,22 +61,67 @@
             this.label4.TabIndex = 39;
             this.label4.Text = "Room Id";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-176, -66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 45);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "List Room";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(421, 98);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(141, 21);
+            this.comboBox2.TabIndex = 48;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(331, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 30);
+            this.label7.TabIndex = 47;
+            this.label7.Text = "Status";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(175, 99);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(141, 20);
+            this.textBox4.TabIndex = 46;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 30);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "Room Id";
+            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.textBox3);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
-            this.groupBox1.Location = new System.Drawing.Point(621, 72);
+            this.groupBox1.Location = new System.Drawing.Point(853, 164);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(355, 445);
-            this.groupBox1.TabIndex = 38;
+            this.groupBox1.TabIndex = 44;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Create Room";
             // 
@@ -113,15 +135,15 @@
             this.label6.TabIndex = 33;
             this.label6.Text = "Room Type";
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(30, 74);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 30);
-            this.label2.TabIndex = 32;
-            this.label2.Text = "Room Id";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 30);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Room Id";
             // 
             // comboBox1
             // 
@@ -155,15 +177,15 @@
             this.textBox2.Size = new System.Drawing.Size(141, 20);
             this.textBox2.TabIndex = 27;
             // 
-            // label3
+            // label8
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(38, 137);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 30);
-            this.label3.TabIndex = 26;
-            this.label3.Text = "Floor";
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(38, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 30);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Floor";
             // 
             // textBox1
             // 
@@ -187,10 +209,10 @@
             // 
             this.button3.BackColor = System.Drawing.Color.White;
             this.button3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(488, 2);
+            this.button3.Location = new System.Drawing.Point(720, 94);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(112, 52);
-            this.button3.TabIndex = 37;
+            this.button3.TabIndex = 43;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = false;
             // 
@@ -198,44 +220,46 @@
             // 
             this.button2.BackColor = System.Drawing.Color.White;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(350, 2);
+            this.button2.Location = new System.Drawing.Point(582, 94);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 52);
-            this.button2.TabIndex = 36;
+            this.button2.TabIndex = 42;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(-162, 72);
+            this.listView1.Location = new System.Drawing.Point(70, 164);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(762, 445);
-            this.listView1.TabIndex = 35;
+            this.listView1.TabIndex = 41;
             this.listView1.UseCompatibleStateImageBehavior = false;
             // 
-            // label1
+            // label9
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-176, -66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 45);
-            this.label1.TabIndex = 34;
-            this.label1.Text = "List Room";
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(56, 26);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(161, 45);
+            this.label9.TabIndex = 40;
+            this.label9.Text = "List Room";
             // 
             // LishRoom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1104, 606);
+            this.ClientSize = new System.Drawing.Size(1264, 635);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Name = "LishRoom";
             this.Text = "LishRoom";
@@ -247,24 +271,25 @@
         }
 
         #endregion
-
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label9;
     }
 }
--- ProjectKS/Program.cs
+++ ProjectKS/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectKS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CustomerList());
+            Application.Run(new LishRoom());
         }
     }
 }
